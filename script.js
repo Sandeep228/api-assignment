3165cab22ca351b1ebe0cb57b2f1a81cc2f71d72
Sandeep Kumar Srivastava <56189781+Sandeep228@users.noreply.github.com>
2021-07-29 14:02:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var request =new XMLHttpRequest();
+//2.opening a connection(specify the url)
+request.open('GET', 'https://api.covid19api.com/summary',true);
+//3.sending a connection 
+request.send();
+//4.once the server responded successfully  then we have to process the data
+request.onload = function(){
+ var data = JSON.parse(this.response);
+    console.log(data);
+       
+}
